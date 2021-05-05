7d43395e11a16e4c91ad6371d0f5913b5e68542d
Hilaricus <stephane.hilaricus@gmail.com>
2021-05-05 14:48:08 +0200

    Modification de l'index.php ajout de la fonctionnalité d'ajout de transport

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ if(isset($_GET["action"])){
             require("controller/visiteStageController.php");
             listStage();
         break;
+        case "listeStage":
+            require("controller/ajoutTransport.php");
+            listStage();
+        break;
     }
 } else{
     //page d'accueil
