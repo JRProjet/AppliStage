c9ef7163be56f2a5e5c3035e83c2e157905b695e
Hilaricus <stephane.hilaricus@gmail.com>
2021-05-06 12:00:06 +0200

    modification database.php changement variable pwd en UnMdp

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 $configDatabase = [
     'dbname'  => 'stsjr1615583_2d6rmi',
     'user'    => 'stsjr1615583',
-    'pwd'     => '7r7pq3fn5y',
+    'pwd'     => 'UnMdp',
     'host'    => '91.216.107.183',
     'port'    => '3306',
     'charset' => 'UTF8',
