bd9d2a553f7f14dff83e614ac6f62f3b46480c28
Guyon <guy.alexis60@gmail.com>
2021-05-06 11:30:05 +0200

    liste utilisateur

--- model/ListeUtilisateurManager.php
+++ model/ListeUtilisateurManager.php
@@ -3,7 +3,7 @@
 require_once("Manager.php");
 
 class ListeUtilisateurManager extends Manager{
-    //fonction sur la table utilisateur uniquement (select, insert...)
+    //fonction sur la table utilisateur uniquement (select, insert...).
     public function getListeUtilisateur(){
         $db = $this->dbConnect();
         return $db->query("SELECT nom, prenom FROM utilisateur WHERE ...");
