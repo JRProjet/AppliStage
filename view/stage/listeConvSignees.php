836aba733707dc598b64a676aa5d7587964ef1e2
RCoppe <rafael.coppe.jr@gmail.com>
2021-05-12 16:36:21 +0200

    Erreur de placement d'une ouverture de php

--- view/stage/listeConvSignees.php
+++ view/stage/listeConvSignees.php
@@ -3,10 +3,11 @@
 
 <?php ob_start() ?>
 <h1>Titre de la page</h1>
-echo "<table>";
-echo ("<tr><th>Jour</th><th>Contenu</th><th>Adresse</th><th>Avis</th></tr>");
 <!-- code html de la page-->
-<?php foreach($lesStages AS $uneConvSignees){
+
+<?php echo "<table>";
+echo ("<tr><th>Jour</th><th>Contenu</th><th>Adresse</th><th>Avis</th></tr>");
+foreach($lesStages AS $uneConvSignees){
         echo "<tr><td><strong>$uneConvSignees->jour</strong></td><td>$uneConvSignees->matindebut</td><td>$uneConvSignees->matinfin</td>";
     }
     echo "</table>";
