75646de22553b5239063162eb17aec88654d9d06
RCoppe <rafael.coppe.jr@gmail.com>
2021-05-12 16:27:34 +0200

    Essai tableau

--- view/stage/listeConvSignees.php
+++ view/stage/listeConvSignees.php
@@ -3,9 +3,13 @@
 
 <?php ob_start() ?>
 <h1>Titre de la page</h1>
+echo "<table>";
+echo ("<tr><th>Jour</th><th>Contenu</th><th>Adresse</th><th>Avis</th></tr>");
 <!-- code html de la page-->
 <?php foreach($lesStages AS $uneConvSignees){
-    //traitement
+        echo "<tr><td><strong>$uneConvSignees->jour</strong></td><td>$uneConvSignees->matindebut</td><td>$uneConvSignees->matinfin</td>";
+    }
+    echo "</table>";
 }?>
 
 
