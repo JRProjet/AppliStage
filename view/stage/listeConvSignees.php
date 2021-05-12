1f2cf0fb264ce0ed94dd49f54a06e57f192a593e
RCoppe <rafael.coppe.jr@gmail.com>
2021-05-12 15:21:45 +0200

    Ajout d'un titre test dans la vue

--- view/stage/listeConvSignees.php
+++ view/stage/listeConvSignees.php
@@ -2,7 +2,7 @@
 <?php $title = "Liste Conventions Signées"?>
 
 <?php ob_start() ?>
-
+<h1>Titre de la page</h1>
 <!-- code html de la page-->
 <?php foreach($lesStages AS $uneConvSignees){
     //traitement
