a99f0944e7f815985bf3c9f8cf2470ec2eb26400
RCoppe <rafael.coppe.jr@gmail.com>
2021-05-12 11:34:07 +0200

    Changement du lien de database.php

--- model/Manager.php
+++ model/Manager.php
@@ -5,7 +5,7 @@ abstract class Manager{
 		// le fichier de configuration pour les accès à la bdd doit être inclus dans la fonction 
 		// sans quoi la variable $configDatabase ne sera pas accessible dans cette dernière 
 		// en raison de sa portée (voir cours vidéo sur la portée des variables)
-		require_once '../config/database.php';
+		require_once 'C:/wamp64\www/AppliStage/config/database.php';
 		$dsn = "mysql:host=".$configDatabase['host'].";port=".$configDatabase['port'].";dbname=".$configDatabase['dbname'].";charset=".$configDatabase['charset'];
 		$db = new PDO($dsn, $configDatabase['user'], $configDatabase['pwd']);
         return $db;
