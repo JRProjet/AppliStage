93bc33ec7c832be8530d4e4ccbab37c168ce227f
Rambaud Evan <evan.rambaud@gmail.com>
2021-05-20 09:53:56 +0200

    ajout du boutton d'ajout

--- view/visiteStage/ajoutVisiteStage.php
+++ view/visiteStage/ajoutVisiteStage.php
@@ -6,80 +6,83 @@
 <!-- code html de la page-->
 <h2>Ajout d'une visite de stage</h2>
 <form action="http://applistage/index.php?action=ajoutVisiteStageTrait" method="post">
-<div>
-    <h3>Date et heure de la visite</h3>
     <div>
-        <label for="date">Date : </label>
-        <input name="date" type="date" required>
+        <h3>Date et heure de la visite</h3>
+        <div>
+            <label for="date">Date : </label>
+            <input name="date" type="date" required>
+        </div>
+        <div>
+            <label for="heureDebut">Heure début de la visite : </label>
+            <input name="heureDebut" type="time">
+        </div>
+        <div>
+            <label for="heureFin">Heure fin de la visite : </label>
+            <input name="heureFin" type="time">
+        </div>
     </div>
     <div>
-        <label for="heureDebut">Heure début de la visite : </label>
-        <input name="heureDebut" type="time">
+        <h3>Informations sur le stage</h3>
+        <div>
+            <label for="etudiant">Nom de l'étudiant : </label>
+            <input type="hidden" name="etudiant" id="etudiant-hidden">
+            <input list="etudiantList" id="etudiant" required>
+            <datalist id="etudiantList">
+                <option data-value=0>Test</option>
+            </datalist>
+        </div>
+        <div>
+            <label for="professeur">Nom du professeur : </label>
+            <input type="hidden" name="professeur" id="professeur-hidden">
+            <input list="professeurList" id="professeur" required>
+            <datalist id="professeurList">
+                <option data-value=0>Test</option>
+            </datalist>
+        </div>
     </div>
     <div>
-        <label for="heureFin">Heure fin de la visite : </label>
-        <input name="heureFin" type="time">
-    </div>
-</div>
-<div>
-    <h3>Informations sur le stage</h3>
-    <div>
-        <label for="etudiant">Nom de l'étudiant : </label>
-        <input type="hidden" name="etudiant" id="etudiant-hidden">
-        <input list="etudiantList" id="etudiant" required>
-        <datalist id="etudiantList">
-            <option data-value=0>Test</option>
-        </datalist>
-    </div>
-    <div>
-        <label for="professeur">Nom du professeur : </label>
-        <input type="hidden" name="professeur" id="professeur-hidden">
-        <input list="professeurList" id="professeur" required>
-        <datalist id="professeurList">
-            <option data-value=0>Test</option>
-        </datalist>
-    </div>
-</div>
-<div>
-    <h3>Informations sur le transport</h3>
-    <div>
-        <label for="transport">Moyen de transport : </label>
-        <input type="hidden" name="transport" id="transport-hidden">
-        <input list="transportList" id="transport" required>
-        <datalist id="transportList">
-            <option data-value=0>Test</option>
-        </datalist>
+        <h3>Informations sur le transport</h3>
+        <div>
+            <label for="transport">Moyen de transport : </label>
+            <input type="hidden" name="transport" id="transport-hidden">
+            <input list="transportList" id="transport" required>
+            <datalist id="transportList">
+                <option data-value=0>Test</option>
+            </datalist>
+        </div>
+        <div>
+            <label for="heureDepart">Heure de départ : </label>
+            <input name="heureDepart" type="time">
+        </div>
+        <div>
+            <label for="heureRetour">Heure de retour : </label>
+            <input name="heureRetour" type="time">
+        </div>
     </div>
     <div>
-        <label for="heureDepart">Heure de départ : </label>
-        <input name="heureDepart" type="time">
+        <button type="submit">Ajouter la visite</button>
     </div>
-    <div>
-        <label for="heureRetour">Heure de retour : </label>
-        <input name="heureRetour" type="time">
-    </div>
-</div>
 </form>
 
 <script>
-document.querySelector('input[list]').addEventListener('input', function(e) {
-    var input = e.target,
-        list = input.getAttribute('list'),
-        options = document.querySelectorAll('#' + list + ' option'),
-        hiddenInput = document.getElementById(input.getAttribute('id') + '-hidden'),
-        inputValue = input.value;
+    document.querySelector('input[list]').addEventListener('input', function(e) {
+        var input = e.target,
+            list = input.getAttribute('list'),
+            options = document.querySelectorAll('#' + list + ' option'),
+            hiddenInput = document.getElementById(input.getAttribute('id') + '-hidden'),
+            inputValue = input.value;
 
-    hiddenInput.value = inputValue;
+        hiddenInput.value = inputValue;
 
-    for(var i = 0; i < options.length; i++) {
-        var option = options[i];
+        for (var i = 0; i < options.length; i++) {
+            var option = options[i];
 
-        if(option.innerText === inputValue) {
-            hiddenInput.value = option.getAttribute('data-value');
-            break;
+            if (option.innerText === inputValue) {
+                hiddenInput.value = option.getAttribute('data-value');
+                break;
+            }
         }
-    }
-});
+    });
 </script>
 
 <!-- affichage de la page -->
