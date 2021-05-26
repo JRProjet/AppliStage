14de345486e4451a2e3ad6d8d80ffc5c8514af78
RCoppe <rafael.coppe.jr@gmail.com>
2021-05-26 11:45:09 +0200

    Amélioration de la requête SQL des conventions signées

--- model/StageManager.php
+++ model/StageManager.php
@@ -10,6 +10,13 @@ class StageManager extends Manager{
     }
     public function getLesConvSignees(){
         $db = $this->dbConnect();
-        return $db->query("SELECT stage.id as ID, contenu, rue_lieu_stage FROM stage JOIN etat ON etat.id = stage.id_etat WHERE etat.libelle = 'Signée'");
+        return $db->query("SELECT utilisateur.nom AS nom, utilisateur.prenom AS prenom, contenu, rue_lieu_stage, 
+        tuteur.nom AS nomTuteur, tuteur.prenom AS prenomTuteur, ville.code_postal AS codePostalEntreprise, ville.nom AS nomVilleEntreprise FROM stage 
+        JOIN etat ON etat.id = stage.id_etat 
+        JOIN utilisateur ON utilisateur.id = stage.id_etudiant
+        JOIN tuteur ON tuteur.id = stage.id_tuteur
+        JOIN entreprise ON entreprise.id = stage.id_entreprise
+        JOIN ville on ville.numero_insee = entreprise.id_numero_insee
+        WHERE etat.libelle = 'Signée' AND utilisateur.id_profil = 2");
     }
 }
--- view/stage/consultConvSignees.php
+++ view/stage/consultConvSignees.php
@@ -6,9 +6,9 @@
 <!-- code html de la page-->
 
 <?php echo "<table>";
-echo ("<tr><th>ID</th><th>Contenu</th><th>Adresse</th></tr>");
+echo ("<tr><th>Identité du stagiaire</th><th>Contenu du stage</th><th>Adresse</th><th>Identité du tuteur</th></tr>");
 foreach($lesStages AS $uneConvSignees){
-        echo "<tr><td>$uneConvSignees->ID</td><td>$uneConvSignees->contenu</td><td>$uneConvSignees->rue_lieu_stage</td></tr>";
+        echo "<tr><td>$uneConvSignees->nom $uneConvSignees->prenom</td><td>$uneConvSignees->contenu</td><td>$uneConvSignees->rue_lieu_stage, $uneConvSignees->codePostalEntreprise $uneConvSignees->nomVilleEntreprise</td><td>$uneConvSignees->nomTuteur $uneConvSignees->prenomTuteur</td></tr>";
     }
     echo "</table>";
 ?>
