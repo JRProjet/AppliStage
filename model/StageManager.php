79c64041b58b306bebb386542f6569f0d419b912
RCoppe <rafael.coppe.jr@gmail.com>
2021-05-26 14:06:13 +0200

    Ajout de la section de l'éleve

--- model/StageManager.php
+++ model/StageManager.php
@@ -10,11 +10,12 @@ class StageManager extends Manager{
     }
     public function getLesConvSignees(){
         $db = $this->dbConnect();
-        return $db->query("SELECT UE.nom AS nom, UE.prenom AS prenom, UP.nom AS nomProf, UP.prenom AS prenomProf, contenu, entreprise.nom AS nomEntreprise, rue_lieu_stage, 
+        return $db->query("SELECT UE.nom AS nom, UE.prenom AS prenom, UP.nom AS nomProf, UP.prenom AS prenomProf, section.libelle AS nomSection, section.categorie AS categSection, section.annee AS anneeSection, contenu, entreprise.nom AS nomEntreprise, rue_lieu_stage, 
         tuteur.nom AS nomTuteur, tuteur.prenom AS prenomTuteur, ville.code_postal AS codePostalEntreprise, ville.nom AS nomVilleEntreprise FROM stage 
         JOIN etat ON etat.id = stage.id_etat 
         JOIN utilisateur UE ON UE.id = stage.id_etudiant
         JOIN utilisateur UP ON UP.id = stage.id_prof_resp_suivi
+        JOIN section on UE.id_section = section.id
         JOIN tuteur ON tuteur.id = stage.id_tuteur
         JOIN entreprise ON entreprise.id = stage.id_entreprise
         JOIN ville on ville.numero_insee = entreprise.id_numero_insee
--- view/stage/consultConvSignees.php
+++ view/stage/consultConvSignees.php
@@ -6,9 +6,9 @@
 <!-- code html de la page-->
 
 <?php echo "<table>";
-echo ("<tr><th>Identité du stagiaire</th><th>Professeur référent</th><th>Contenu du stage</th><th>Nom de l'entreprise</th><th>Adresse</th><th>Identité du tuteur</th></tr>");
+echo ("<tr><th>Identité du stagiaire</th><th>Professeur référent</th><th>Section de l'élève</th><th>Contenu du stage</th><th>Nom de l'entreprise</th><th>Adresse</th><th>Identité du tuteur</th></tr>");
 foreach($lesStages AS $uneConvSignees){
-        echo "<tr><td>$uneConvSignees->nom $uneConvSignees->prenom</td><td>$uneConvSignees->nomProf $uneConvSignees->prenomProf</td><td>$uneConvSignees->contenu</td><td>$uneConvSignees->nomEntreprise</td><td>$uneConvSignees->rue_lieu_stage, $uneConvSignees->codePostalEntreprise $uneConvSignees->nomVilleEntreprise</td><td>$uneConvSignees->nomTuteur $uneConvSignees->prenomTuteur</td></tr>";
+        echo "<tr><td>$uneConvSignees->nom $uneConvSignees->prenom</td><td>$uneConvSignees->nomProf $uneConvSignees->prenomProf</td><td>$uneConvSignees->nomSection Option $uneConvSignees->categSection - Année $uneConvSignees->anneeSectiontd><td>$uneConvSignees->contenu</td><td>$uneConvSignees->nomEntreprise</td><td>$uneConvSignees->rue_lieu_stage, $uneConvSignees->codePostalEntreprise $uneConvSignees->nomVilleEntreprise</td><td>$uneConvSignees->nomTuteur $uneConvSignees->prenomTuteur</td></tr>";
     }
     echo "</table>";
 ?>
