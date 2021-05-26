40cddb24d8063e8591c32b85926647479e3dcbda
Nicolas Congiu <congiu.nicolas@outlook.com>
2021-05-26 09:45:21 +0200

    modification du nom Class ConsultEntrepriseTechnologieManager

--- model/consultEntrepriseManager.php
+++ model/consultEntrepriseManager.php
@@ -3,7 +3,7 @@
 require_once("Manager.php");
 
 class ConsultEntrepriseTechnologieManager extends Manager{
-    //fonction sur la table viste stage uniquement (select, insert...)
+    //fonction sur la table entreprise uniquement (select, insert...)
     public function getlesEntreprisesTechnologie(){
         $db = $this->dbConnect();
         return $db->query("SELECT entreprise.nom, rue, ville.nom, ville.code_postal, technologie.libelle from entreprise join ville on id_numero_insee = ville.numero_insee join stage on entreprise.id = id_entreprise join stage_tech on stage.id=id_stage join technologie on id_technologie=technologie.id");
