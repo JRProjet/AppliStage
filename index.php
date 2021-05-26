1d6de300661f9f98608898f9088f62e41a66048f
RCoppe <rafael.coppe.jr@gmail.com>
2021-05-26 11:02:28 +0200

    Pull sur la branche master

--- index.php
+++ index.php
@@ -4,18 +4,13 @@ if (isset($_GET["action"])) {
     switch ($_GET["action"]) {
         case "listeStage":
             require("controller/visiteStageController.php");
-<<<<<<< HEAD
             listeStage();
         break;
+        
         case "consultConvSignees":
             require("controller/stageController.php");
             consultConvSignees();
         break;
-=======
-            listStage();
-            break;
-        default;
->>>>>>> e1cdd88623c6db5e450e3c698e0b05ae8491948b
     }
 } else {
     //page d'accueil
--- model/Manager.php
+++ model/Manager.php
@@ -7,22 +7,12 @@ abstract class Manager
 		// le fichier de configuration pour les accès à la bdd doit être inclus dans la fonction 
 		// sans quoi la variable $configDatabase ne sera pas accessible dans cette dernière 
 		// en raison de sa portée (voir cours vidéo sur la portée des variables)
-<<<<<<< HEAD
-		require_once 'C:/wamp64\www/AppliStage/config/database.php';
-		$dsn = "mysql:host=".$configDatabaseDev['host'].";port=".$configDatabaseDev['port'].";dbname=".$configDatabaseDev['dbname'].";charset=".$configDatabaseDev['charset'];
-=======
 		require_once $_SERVER["DOCUMENT_ROOT"] . '/config/database.php';
 		$dsn = "mysql:host=" . $configDatabaseDev['host'] . ";port=" . $configDatabaseDev['port'] . ";dbname=" . $configDatabaseDev['dbname'] . ";charset=" . $configDatabaseDev['charset'];
->>>>>>> e1cdd88623c6db5e450e3c698e0b05ae8491948b
 		$db = new PDO($dsn, $configDatabaseDev['user'], $configDatabaseDev['pwd']);
 		$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 		// Activation des erreurs PDO
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-<<<<<<< HEAD
-        return $db;
-    }
-}
-=======
 		}
 		catch(Exception $err){
 			print_r($err);
@@ -30,4 +20,3 @@ abstract class Manager
 		return $db;
 	}
 }
->>>>>>> e1cdd88623c6db5e450e3c698e0b05ae8491948b
